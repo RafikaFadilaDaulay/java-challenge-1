2ee41f86dbc2d3d5a05b8e2d6c649f8236c6e97a
RafikaFadilaDaulay <91516903+RafikaFadilaDaulay@users.noreply.github.com>
2021-10-12 20:19:47 +0700

    Add files via upload

---
+++ BasicJava.java
@@ -0,0 +1,32 @@
+public class BasicJava{
+    public static void main(String[] args) {
+
+    // mendeklarasikan variabel 
+    String first, last, alamat, hobi, hp;
+    int umur;
+    double berat, tinggi;
+    char goldar;
+
+    // mendeklarasikan nilai
+    first = "Rafika" ;
+    last = "Fadila Daulay";
+    alamat = "Medan";
+    hobi = " memasak" ;
+    hp = "081256372512";
+    umur = 21;
+    berat = 63.22;
+    tinggi = 158.22;
+    goldar = 'O';
+
+    // menampilkan output
+    System.out.println("Nama : " + first + last);
+    System.out.println("Alamat : " + alamat);
+    System.out.println("No hp : " + hp);
+    System.out.println("Hobi : " + hobi);
+    System.out.println("Umur : " + umur);
+    System.out.println("Berat : " + berat + " kg");
+    System.out.println("Tinggi : " + tinggi + " cm");
+    System.out.println("Golongan Darah : " + goldar);
+ 
+}
+}
